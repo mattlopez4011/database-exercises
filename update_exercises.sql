db93c6482c48942077bf47d91a2b94d79f6e4cec
Matt Lopez <matthew.lopez4011@gmail.com>
2019-10-10 16:56:24 -0500

    Done with update exercise lecture

--- update_exercises.sql
+++ update_exercises.sql
@@ -9,14 +9,12 @@ SELECT * FROM albums;
 SELECT * FROM albums WHERE release_date < 1980;
 -- Move all the albums before 1980 back to the 1800s.
 UPDATE albums
-SET release_date = release_date - 100
-WHERE release_date < 1980;
+SET release_date = release_date - 100 WHERE release_date < 1980;
 SELECT * FROM albums WHERE release_date < 1980;
 
 -- All albums by Michael Jackson
 SELECT * FROM albums WHERE artist = 'Michael Jackson';
 -- Change 'Michael Jackson' to 'Peter Jackson'
 UPDATE albums
-SET artist = 'Peter Jackson'
-WHERE artist = 'Michael Jackson';
+SET artist = 'Peter Jackson' WHERE artist = 'Michael Jackson';
 SELECT * FROM albums WHERE artist = 'Peter Jackson';
